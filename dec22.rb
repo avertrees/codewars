76adbd32a50c4e224b540ed8c06cd51de2dfe023
Alessandra Vertrees <avertrees447@gmail.com>
2019-12-22 09:17:27 -0500

    code2

--- dec22.rb
+++ dec22.rb
@@ -21,3 +21,40 @@ def highest_rank(arr)
  
   nums.keys.last
 end
+
+def longest(s)
+  # your code here
+  puts s
+  sub = []
+  substrings = {}
+  if (s.length == 1)
+    substrings[s] = s.length
+  else
+    s.each_byte do |c|
+      if(sub.length == 0)
+        sub.push(c.chr)
+      elsif(c - sub.last.ord >= 0)
+      
+        sub.push(c.chr)
+
+        if (sub.last.ord == c)
+          s = sub.join.to_s
+          substrings[s] = s.length
+        end
+      else 
+        s = sub.join.to_s
+        substrings[s] = s.length
+        
+        sub.clear
+        
+        sub.push(c.chr) 
+
+      end
+   
+    end
+  end
+
+    max = substrings.values.max
+    longest = substrings.select{|k,v| v === max}
+    longest.keys[0] 
+end
